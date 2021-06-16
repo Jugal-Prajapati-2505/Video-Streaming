eed12e3ed23378423b832ce9db34db33311b31cd
Jugal-Prajapati-2505 <jugalprajapati2017@gmail.com>
2021-06-16 14:48:45 +0530

    Move private info into dev.js

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Jugal:FDSpqU1e4mLeTJyI@video-streaming.lv3gs.mongodb.net/test?retryWrites=true&w=majority',
-    {useNewUrlParser: true , useUnifiedTopology: true}).then(() =>console.log('DB Connect'))
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,
+    {useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex:true}).then(() =>console.log('DB Connect'))
                             .catch(err => console.error(err));
 
-app.get('/', (req,res)=>{
-    res.send('Hello Jugal')
-});
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser()); 
+
+/* app.get('/', (req,res)=>{
+    res.send('Hello Jugal how are you')
+}); */ 
+
+app.post('/api/users/register',(req, res) => {
+        const user = new User(req.body)
+        
+        user.save((err, userData) =>{
+            if(err) return res.json ({ success : false, err })
+            return res.status(200).json({
+                success:true
+            })
+        })
+})
 
 app.listen(5000);
 
